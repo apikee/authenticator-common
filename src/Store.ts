17c4e99660172148f12cfee1b41e285280b5d04f
Jakub Tomáš <jakubtomas108@gmail.com>
2022-07-16 21:56:11 +0200

    Update types

--- src/Store.ts
+++ src/Store.ts
@@ -1,10 +1,14 @@
 import { JwtPayload } from "jsonwebtoken";
 
 export class Store {
-  addToken!: (token: string, subject: string, replace?: boolean) => void;
+  addToken!: (
+    token: string,
+    subject: string,
+    replace?: boolean
+  ) => void | Promise<void>;
   findSubjectByToken!: (token: string) => string | Promise<string>;
-  deleteToken!: (token: string) => void | Promise<string>;
-  deleteAllTokensForSubject!: (subject: string) => void | Promise<string>;
+  deleteToken!: (token: string) => void | Promise<void>;
+  deleteAllTokensForSubject!: (subject: string) => void | Promise<void>;
   clearExpiredTokens!: (
     validateToken: (
       type: "access" | "refresh",
