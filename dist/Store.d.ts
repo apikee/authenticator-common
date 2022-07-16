f0664db733daaa9a6ccaa606bea571d0ff8b4def
Jakub Tomáš <jakubtomas108@gmail.com>
2022-07-16 21:56:27 +0200

    Add build

--- dist/Store.d.ts
+++ dist/Store.d.ts
@@ -1,8 +1,8 @@
 import { JwtPayload } from "jsonwebtoken";
 export declare class Store {
-    addToken: (token: string, subject: string, replace?: boolean) => void;
+    addToken: (token: string, subject: string, replace?: boolean) => void | Promise<void>;
     findSubjectByToken: (token: string) => string | Promise<string>;
-    deleteToken: (token: string) => void | Promise<string>;
-    deleteAllTokensForSubject: (subject: string) => void | Promise<string>;
+    deleteToken: (token: string) => void | Promise<void>;
+    deleteAllTokensForSubject: (subject: string) => void | Promise<void>;
     clearExpiredTokens: (validateToken: (type: "access" | "refresh", token: string) => JwtPayload | string | null) => Promise<void>;
 }
